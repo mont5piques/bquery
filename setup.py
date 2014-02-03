e9c6ee9ec4509b3ef769248d700a3fd25162bdae
Adam Cécile <acecile@mandriva.com>
2014-02-03 17:01:27 +0100

    Add setup.py and manpage

---
+++ setup.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python
+
+from setuptools import setup
+
+setup( \
+    name='bquery',
+    version='0.1',
+    description='bQuery is a simple tool to quickly parse html streams with a jQuery-like expression',
+    author='mont5piques',
+    author_email='newishak@gmail.com',
+    license = "WTFPL",
+    url='http://github.com/mont5piques/bquery',
+    scripts=['bquery'],
+    install_requires=['pyquery>=1.2'],
+)
